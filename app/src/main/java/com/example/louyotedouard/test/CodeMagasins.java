2e417ea222316d849abc218d26cf7b7df65b9c43
LOUYOT Edouard <edouard88100@aim.com>
2014-12-22 22:26:31 +0100

    vue saisis code magasins

--- app/src/main/java/com/example/louyotedouard/test/CodeMagasins.java
+++ app/src/main/java/com/example/louyotedouard/test/CodeMagasins.java
@@ -1,19 +1,39 @@
 package com.example.louyotedouard.test;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class CodeMagasins extends ActionBarActivity {
 
+    Button btn_launch;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_code_magasins);
     }
+    public void addListenerOnButton() {
 
+        btn_launch = (Button) findViewById(R.id.btn_magasins);
+
+        btn_launch.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent browserIntent =
+                        new Intent(CodeMagasins.this, choixMode.class);
+                startActivity(browserIntent);
+
+            }
+
+        });
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
