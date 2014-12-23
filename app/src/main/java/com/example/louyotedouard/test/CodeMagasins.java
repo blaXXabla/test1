0848353b17f39e51bce94617307ba36f81d1d2ec
Xavier <xav.antoine57@gmail.com>
2014-12-23 13:52:38 +0100

    test

--- app/src/main/java/com/example/louyotedouard/test/CodeMagasins.java
+++ app/src/main/java/com/example/louyotedouard/test/CodeMagasins.java
@@ -8,12 +8,14 @@ import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
-
+import android.widget.EditText;
 
 
 public class CodeMagasins extends ActionBarActivity {
 
-    Button btn_launch;
+    private Button btn_launch;
+    private EditText et_codemagasin;
+    private Button btn_effacer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +25,18 @@ public class CodeMagasins extends ActionBarActivity {
     public void addListenerOnButton() {
 
         btn_launch = (Button) findViewById(R.id.btn_magasins);
+        btn_effacer = (Button) findViewById(R.id.btn_effacer);
+        et_codemagasin = (EditText) findViewById(R.id.et_codeMagasins);
+        Button bt0 = (Button) findViewById(R.id.btn_code0);
+        Button bt1 = (Button) findViewById(R.id.btn_code1);
+        Button bt2 = (Button) findViewById(R.id.btn_code2);
+        Button bt3 = (Button) findViewById(R.id.btn_code3);
+        Button bt4 = (Button) findViewById(R.id.btn_code4);
+        Button bt5 = (Button) findViewById(R.id.btn_code5);
+        Button bt6 = (Button) findViewById(R.id.btn_code6);
+        Button bt7 = (Button) findViewById(R.id.btn_code7);
+        Button bt8 = (Button) findViewById(R.id.btn_code8);
+        Button bt9 = (Button) findViewById(R.id.btn_code9);
 
         btn_launch.setOnClickListener(new View.OnClickListener() {
 
@@ -33,6 +47,107 @@ public class CodeMagasins extends ActionBarActivity {
                         new Intent(CodeMagasins.this, choixMode.class);
                 startActivity(browserIntent);
 
+            }
+
+        });
+
+        btn_effacer.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                et_codemagasin.setText(et_codemagasin.getText().subSequence(0,et_codemagasin.length()-1));
+
+            }
+
+        });
+
+        bt0.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                et_codemagasin.setText(et_codemagasin.getText()+"0");
+            }
+
+        });
+
+        bt1.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                et_codemagasin.setText(et_codemagasin.getText()+"1");
+            }
+
+        });
+
+        bt2.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                et_codemagasin.setText(et_codemagasin.getText()+"2");
+            }
+
+        });
+
+        bt3.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                et_codemagasin.setText(et_codemagasin.getText()+"3");
+            }
+
+        });
+
+        bt4.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                et_codemagasin.setText(et_codemagasin.getText()+"4");
+            }
+
+        });
+
+        bt5.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                et_codemagasin.setText(et_codemagasin.getText()+"5");
+            }
+
+        });
+
+        bt6.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                et_codemagasin.setText(et_codemagasin.getText()+"6");
+            }
+
+        });
+
+        bt7.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                et_codemagasin.setText(et_codemagasin.getText()+"7");
+            }
+
+        });
+
+        bt8.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                et_codemagasin.setText(et_codemagasin.getText()+"8");
+            }
+
+        });
+
+        bt9.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                et_codemagasin.setText(et_codemagasin.getText()+"9");
             }
 
         });
