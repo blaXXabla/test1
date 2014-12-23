57a6445636c12b0cb93174644605f343c391345c
Xavier <xav.antoine57@gmail.com>
2014-12-23 12:04:15 +0100

    test

---
+++ app/src/main/java/com/example/louyotedouard/test/Connexion.java
@@ -0,0 +1,74 @@
+package com.example.louyotedouard.test;
+
+import android.os.AsyncTask;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+/**
+ * Created by Xavier on 23/12/2014.
+ */
+public class Connexion {
+    private String jsonResult;
+    private String url = "http://cpriyankara.coolpage.biz/employee_details.php";
+
+
+
+    // Async Task to access the web
+    private class JsonReadTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... params) {
+            HttpClient httpclient = new DefaultHttpClient();
+            HttpPost httppost = new HttpPost(params[0]);
+            try {
+                HttpResponse response = httpclient.execute(httppost);
+                jsonResult = inputStreamToString(
+                        response.getEntity().getContent()).toString();
+            }
+
+            catch (ClientProtocolException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        private StringBuilder inputStreamToString(InputStream is) {
+            String rLine = "";
+            StringBuilder answer = new StringBuilder();
+            BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+
+            try {
+                while ((rLine = rd.readLine()) != null) {
+                    answer.append(rLine);
+                }
+            }
+
+            catch (IOException e) {
+                // e.printStackTrace();
+                System.out.println("Error..." + e.toString());
+            }
+            return answer;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+
+        }
+    }// end async task
+
+    public void accessWebService() {
+        JsonReadTask task = new JsonReadTask();
+        // passes values for the urls string array
+        task.execute(new String[] { url });
+    }
+}
